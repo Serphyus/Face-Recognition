6216b67982dc945859eac2be49913eeff0ef4e02
Serphyus <serphyus@gmail.com>
2021-02-05 23:56:07 +0100

    added rectangle drawing and name display functionality

--- source/FaceRecognition.py
+++ source/FaceRecognition.py
@@ -30,25 +30,40 @@ class FaceRecognition:
             if True in results:
                 name = self.database.Users[results.index(True)].name
             
-            print(name)
             matches.append([name, face_locations[index]])
         
         return matches
 
 
-    def mainLoop(self) -> None:
-        cap = cv2.VideoCapture(0)
+    def mainLoop(self, camera_type='integrated') -> None:
+        if camera_type == 'integrated':
+            cap = cv2.VideoCapture(0)
+        elif camera_type == 'external':
+            cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)
+        else:
+            raise ValueError('camera_type argument must be integrated or external')
+        
+        if self.config.draw_name:
+            fontFace = getattr(cv2, self.config.fontFace)
+            fontScale = self.config.fontScale
+            rectangleOffset = self.config.rectangleOffset
         
         while True:
             _, frame = cap.read()
             matches = self.getFacematch(frame)
 
             for name, location in matches:
-                
-                if self.config.draw_rectangle:
-                    for (top, right, bottom, left) in [location]:
+                for (top, right, bottom, left) in [location]:
+                    if self.config.draw_rectangle:
                         cv2.rectangle(frame, (left, top), (right, bottom), (250, 250, 250), 1)
             
+                    if self.config.draw_name:
+                        textSize, baseLine = cv2.getTextSize(name, fontFace, fontScale, 1)
+                        if (right - left) < textSize[0]: right = (left + textSize[0] + rectangleOffset)
+
+                        cv2.rectangle(frame, (left, bottom), (right, bottom + 25), (250, 250, 250), -1)
+                        cv2.putText(frame, name, (left + rectangleOffset, bottom + 20), fontFace, fontScale, (0, 0, 0), 1, cv2.LINE_AA)
+                    
             cv2.imshow(self.config.window_name, frame)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
@@ -61,4 +76,4 @@ class FaceRecognition:
 
 if __name__ == "__main__":
     fr = FaceRecognition(os.path.abspath(os.path.dirname(__file__)))
-    fr.mainLoop()
+    fr.mainLoop(camera_type='external')
